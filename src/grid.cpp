5bc3ea94e55c1041a3c747805ff9b5505ef1c9e4
Emie <emie.lafourcade@etu.univ-lyon1.fr>
2020-12-02 15:16:04 +0100

    petites fonctions utiles

--- src/grid.cpp
+++ src/grid.cpp
@@ -8,4 +8,27 @@ grid::grid(const int C, const int L)
 
     for (int i = 0; i < L * C; i++)
         g.push_back(nouveau);
+
+    col=C;
+    line=L;
+}
+
+void grid::modifhauteur(const int i, const int j, const int h)
+{
+    g.at(i*col+j)->hauteur=h;
+}
+
+int grid::indice(const int i, const int j)
+{
+    return i*col+j;
+}
+
+int grid::colonne (const int indice)
+{
+    return indice%col;
+}
+
+int grid::ligne (const int indice)
+{
+    return indice/col;
 }
--- src/grid.h
+++ src/grid.h
@@ -19,8 +19,17 @@ public:
     //Destructeur---------------------------------------------------------------
     ~grid();
 
+    void modifhauteur(const int C, const int L, const int h);
+
+    int indice(const int i, const int j);
+
+    int ligne (const int indice);
+    int colonne (const int indice);
+    int hauteur (const int indice);
+    
 private:
     std::vector<noeud *> g;
+    int col, line;
 };
 
 #endif
