abfcb15526286add3b67c4140968d4a97249b99f
Emie <emie.lafourcade@etu.univ-lyon1.fr>
2020-12-02 16:25:51 +0100

    fonctions de base

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "grid.h"
 
-grid::grid(const int C, const int L)
+grid::grid(const int L, const int C)
 {
     for (int i = 0; i < L * C; i++)
     {
@@ -23,75 +23,154 @@ grid::~grid()
 
 void grid::affichage()
 {
+    int n = 0;
     for (int i = 0; i < line; i++)
     {
         for (int j = 0; j < col; j++)
-            std::cout << g.at(i)->hauteur << "  ";
+        {
+            std::cout << g.at(n)->hauteur << "  ";
+            n++;
+        }
+
         std::cout << std::endl;
     }
 }
 
 void grid::modifhauteur(const int i, const int j, const int h)
 {
-    g.at(i * col + j)->hauteur = h;
+    if (i < line && j < col && i >= 0 && j >= 0)
+        g.at(indice(i, j))->hauteur = h;
+    else
+        std::cout << "ce noeud n'existe pas" << std::endl;
 }
 
 int grid::indice(const int i, const int j)
 {
-    return i * col + j;
+    if (i < line && j < col && i >= 0 && j >= 0)
+        return i * col + j;
+    else
+        return -1;
 }
 
 int grid::colonne(const int indice)
 {
-    return indice % col;
+    if ((unsigned int)indice < g.size() && indice >= 0)
+        return indice % col;
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return -1;
+    }
 }
 
 int grid::ligne(const int indice)
 {
-    return indice / col;
+    if ((unsigned int)indice < g.size() && indice >= 0)
+        return indice / col;
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return -1;
+    }
 }
 
 int grid::hauteur(const int indice)
 {
-    return g.at(indice)->hauteur;
+    if ((unsigned int)indice < g.size() && indice >= 0)
+        return g.at(indice)->hauteur;
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return -1;
+    }
 }
 
 bool grid::existNord(const int i)
 {
-    return i - 1 >= 0;
+    if (i < line && i >= 0)
+        return i - 1 >= 0;
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return false;
+    }
 }
 
 bool grid::existSud(const int i)
 {
-    return i + 1 < line;
+    if (i < line && i >= 0)
+        return i + 1 < line;
+
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return false;
+    }
 }
 
 bool grid::existEst(const int j)
 {
-    return j + 1 < col;
+    if (j < col && j >= 0)
+        return j + 1 < col;
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return false;
+    }
 }
 
 bool grid::existOuest(const int j)
 {
-    return j - 1 >= 0;
+    if (j < col && j >= 0)
+        return j - 1 >= 0;
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return false;
+    }
 }
 
 int grid::indiceNord(const int i, const int j)
 {
-    return (i - 1) * col + j;
+    if (i < line && j < col && i >= 0 && j >= 0)
+        return (i - 1) * col + j;
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return -1;
+    }
 }
 
 int grid::indiceSud(const int i, const int j)
 {
-    return (i + 1) * col + j;
+    if (i < line && j < col && i >= 0 && j >= 0)
+        return (i + 1) * col + j;
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return -1;
+    }
 }
 
 int grid::indiceEst(const int i, const int j)
 {
-    return i * col + (j + 1);
+    if (i < line && j < col && i >= 0 && j >= 0)
+        return i * col + (j + 1);
+
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return -1;
+    }
 }
 
 int grid::indiceOuest(const int i, const int j)
 {
-    return i * col + (j - 1);
+    if (i < line && j < col && i >= 0 && j >= 0)
+        return i * col + (j - 1);
+    else
+    {
+        std::cout << "ce noeud n'existe pas" << std::endl;
+        return -1;
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 int main()
 {
     grid grid(3, 4);
+    grid.modifhauteur(2, 2, 5);
     grid.affichage();
     return EXIT_SUCCESS;
 }
