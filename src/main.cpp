31a3f2f200c1a2aaeb53e8a1c6cf497e8c61d53a
Emie <emie.lafourcade@etu.univ-lyon1.fr>
2020-12-13 12:38:42 +0100

    commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -13,139 +13,143 @@
 
 int main(int argc, const char **argv)
 {
-    srand(time(NULL));
-    Grid * grid;
-    string filenameGrid = "None";
-    string filenameSites = "None";
+  srand(time(NULL));
+  Grid *grid;
+  string filenameGrid = "None";
+  string filenameSites = "None";
 
-    try
+  try
+  {
+    if (argv[1] != NULL)
     {
-      if (argv[1] != NULL)
-      {
-        filenameGrid = argv[1];
-      }
+      filenameGrid = argv[1];
+    }
 
-      cout << "------------------------------------------------------\n"
-           << "\tLa grille \n"
-           << "------------------------------------------------------" << endl;
-      cout << "\n";
+    cout << "------------------------------------------------------\n"
+         << "\tLa grille \n"
+         << "------------------------------------------------------" << endl;
+    cout << "\n";
 
-      if(filenameGrid == "None" )
-      {
-        // creation d'une grille avec nb cols et lignes et modif d'hauteur
-        cout << "Grille generée de manière aléatoire: " << ":\n" << endl;
-        int colonnes = (rand() % 10 + 3);
-        int lignes = (rand() % 10 + 3);
-        grid = new Grid(lignes, colonnes);
-        for (int h = 0; h < lignes * colonnes - 1; h++)
-            grid->modifhauteur(grid->ligne(h), grid->colonne(h), rand() % 50);
-        grid->affichage();
-        cout << endl;
-      }
-      else
-      {
-        // creation de la grille à partir du fichier txt
-        cout << "Grille du fichier " << filenameGrid << ":\n" << endl;
-        grid = new Grid(filenameGrid);
-        grid->affichage();
-        cout << endl;
-      }
+    if (filenameGrid == "None")
+    {
+      // creation d'une grille avec nb cols et lignes et modif d'hauteur
+      cout << "Grille generée de manière aléatoire: "
+           << ":\n"
+           << endl;
+      int colonnes = (rand() % 10 + 3);
+      int lignes = (rand() % 10 + 3);
+      grid = new Grid(lignes, colonnes);
+      for (int h = 0; h < lignes * colonnes - 1; h++)
+        grid->modifhauteur(grid->ligne(h), grid->colonne(h), rand() % 50);
+      grid->affichage();
+      cout << endl;
+    }
+    else
+    {
+      // creation de la grille à partir du fichier txt
+      cout << "Grille du fichier " << filenameGrid << ":\n"
+           << endl;
+      grid = new Grid(filenameGrid);
+      grid->affichage();
+      cout << endl;
+    }
 
-      if (argv[1] != NULL && argv[2] != NULL)
-      {
-        filenameSites = argv[2];
-      }
-      else
-      {
-        filenameSites = "None";
-      }
+    if (argv[1] != NULL && argv[2] != NULL)
+    {
+      filenameSites = argv[2];
+    }
+    else
+    {
+      filenameSites = "None";
+    }
 
-      cout << "------------------------------------------------------\n"
-           << "\tLibrairies de la grille \n"
-           << "------------------------------------------------------" << endl;
-      cout << "\n";
+    cout << "------------------------------------------------------\n"
+         << "\tLibrairies de la grille \n"
+         << "------------------------------------------------------" << endl;
+    cout << "\n";
 
-      if(filenameSites == "None")
+    if (filenameSites == "None")
+    {
+      int nbPoints = 4;
+      vector<int> indicesSites;
+      int indice;
+
+      cout << "Indices des sites: " << endl;
+      for (int x = 0; x < nbPoints - 1; x++)
       {
-        int nbPoints = 4;
-        vector<int> indicesSites;
-        int indice;
-
-        cout << "Indices des sites: " << endl;
-        for (int x = 0; x < nbPoints-1; x++)
-        {
-            indice = rand() % ((grid->getLine() * grid->getCol()) - 1);
-            cout << indice << ", ";
-            indicesSites.push_back(indice);
-        }
         indice = rand() % ((grid->getLine() * grid->getCol()) - 1);
-        cout << indice << "\n" << endl;
+        cout << indice << ", ";
         indicesSites.push_back(indice);
+      }
+      indice = rand() % ((grid->getLine() * grid->getCol()) - 1);
+      cout << indice << "\n"
+           << endl;
+      indicesSites.push_back(indice);
 
-        SitesLibrairies librairies(*grid, "", indicesSites);
-        librairies.affichage(*grid);
+      SitesLibrairies librairies(*grid, "", indicesSites);
+      librairies.affichage(*grid);
+    }
+    else
+    {
+      SitesLibrairies librairies(*grid, filenameSites);
+      librairies.affichage(*grid);
+      cout << endl;
+    }
+
+    if (argv[1] != NULL && argv[2] != NULL && argv[3] != NULL && argv[4] != NULL)
+    {
+      int i = atoi(argv[3]);
+      int j = atoi(argv[4]);
+
+      cout << "\nLa hauteur à (" << i << "," << j << ") est: " << grid->hauteur(grid->indice(i, j)) << endl;
+
+      int index = grid->indiceOuest(i, j);
+      if (index != -1 && index != -2)
+      {
+        cout << "\t La hauteur à l'ouest est: " << grid->hauteur(index) << endl;
       }
       else
       {
-        SitesLibrairies librairies(*grid, filenameSites);
-        librairies.affichage(*grid);
-        cout << endl;
+        cout << "\tPas de sommet à l'ouest" << endl;
       }
 
-      if (argv[1] != NULL && argv[2] != NULL && argv[3] != NULL && argv[4] != NULL)
+      index = grid->indiceNord(i, j);
+      if (index != -1 && index != -2)
       {
-        int i = atoi(argv[3]);
-        int j = atoi(argv[4]);
-
-        cout << "\nL'hauteur à (" << i << "," << j << ") est: " << grid->hauteur(grid->indice(i, j)) << endl;
-
-        int index = grid->indiceOuest(i, j);
-        if (index != -1 && index != -2)
-        {
-            cout << "\t L'hauteur à l'ouest est: " << grid->hauteur(index) << endl;
-        }
-        else
-        {
-           cout << "\tPas de sommet à l'ouest" << endl;
-        }
-
-        index = grid->indiceNord(i, j);
-        if (index != -1 && index != -2)
-        {
-            cout << "\t L'hauteur au nord est: " << grid->hauteur(index) << endl;
-        }
-        else
-        {
-           cout << "\tPas de sommet au nord" << endl;
-        }
-
-        index = grid->indiceEst(i, j);
-        if (index != -1 && index != -2)
-        {
-            cout << "\t L'hauteur à l'est est: " << grid->hauteur(index) << endl;
-        }
-        else
-        {
-           cout << "\tPas de sommet à l'est" << endl;
-        }
-
-        index = grid->indiceSud(i, j);
-        if (index != -1 && index != -2)
-        {
-            cout << "\t L'hauteur au sud est: " << grid->hauteur(index) << endl;
-        }
-        else
-        {
-           cout << "\tPas de sommet au sud" << endl;
-        }
+        cout << "\t La hauteur au nord est: " << grid->hauteur(index) << endl;
+      }
+      else
+      {
+        cout << "\tPas de sommet au nord" << endl;
       }
 
-      delete grid;
-    }
-    catch (exception& e)
-    {
-      cerr << "exception caught: " << e.what() << '\n';
+      index = grid->indiceEst(i, j);
+      if (index != -1 && index != -2)
+      {
+        cout << "\t La hauteur à l'est est: " << grid->hauteur(index) << endl;
+      }
+      else
+      {
+        cout << "\tPas de sommet à l'est" << endl;
+      }
+
+      index = grid->indiceSud(i, j);
+      if (index != -1 && index != -2)
+      {
+        cout << "\t La hauteur au sud est: " << grid->hauteur(index) << endl;
+      }
+      else
+      {
+        cout << "\tPas de sommet au sud" << endl;
+      }
     }
 
-    return EXIT_SUCCESS;
+    delete grid;
+  }
+  catch (exception &e)
+  {
+    cerr << "exception caught: " << e.what() << '\n';
+  }
+
+  return EXIT_SUCCESS;
 }
